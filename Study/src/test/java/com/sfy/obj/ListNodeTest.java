1ccef650080e48ee7a64efa564b17fbeab9589e0
fayun.sun <fayun.sun@qunar.com>
2017-05-16 10:37:04 +0800

    测试类

---
+++ Study/src/test/java/com/sfy/obj/ListNodeTest.java
@@ -0,0 +1,17 @@
+package com.sfy.obj;
+
+import lombok.Data;
+
+/**
+ * @Description: ListNodeTest.java
+ * @User: sunfayun
+ * @Date: 2017/04/26
+ * @Version: 1.0
+ */
+@Data
+public class ListNodeTest {
+
+    private int data;
+    private ListNodeTest next;
+
+}
